0dc98196ef8b53714c8beabe43573b0e122a60ac
IreneG <irene.gonza.mor@gmail.com>
2023-03-29 12:14:02 +0200

    PresentacioServiceIMpl hecho

--- src/main/java/com/example/services/PresentacionServiceImpl.java
+++ src/main/java/com/example/services/PresentacionServiceImpl.java
@@ -1,5 +1,48 @@
 package com.example.services;
 
-public class PresentacionServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.PresentacionDao;
+import com.example.entities.Presentacion;
+
+@Service
+public class PresentacionServiceImpl implements PresentacionService {
+
+    /** Creamos la variable de tipo Dao para poder inyectarle la capa DAO, puede resolverse con un @Autowire o mediante constructor: */
+    @Autowired
+    private PresentacionDao presentacionDao;
+
+    @Override
+    public List<Presentacion> findAll(Sort sort) {
+        return presentacionDao.findAll(sort);
+
+    }
+
+    @Override
+    public Page<Presentacion> findAll(Pageable pageable) {
+        
+        return presentacionDao.findAll(pageable);
+    }
+
+    @Override
+    public Presentacion findById(long id) {
+        return presentacionDao.findById(id);
+    }
+
+    @Override
+    public Presentacion save(Presentacion presentacion) {
+       return presentacionDao.save(presentacion);
+    }
+
+    @Override
+    public void delete(Presentacion presentacion) {
+        presentacionDao.delete(presentacion);
+    }
+
 }
